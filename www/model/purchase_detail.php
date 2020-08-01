95f564ffd4e73f0d67c8a721c8156179708aeaf3
gorst2020 <hashiguchi.kaz1219@gmail.com>
2020-08-01 09:58:29 +0900

    #15 不必要なコメントの削除

--- www/model/purchase_detail.php
+++ www/model/purchase_detail.php
@@ -14,16 +14,8 @@ function insert_purchase_detail($db,$history_id,$purchase_price,$amount,$item_id
       )
     VALUES(?,?,?,?)
   ";
-  /*直接値を指定するとエラーが発生しない...しかし、変数を指定するとエラーが発生する...
-  つまり、変数内に適切な数字が入っていない可能性がある。続きは明日から。 */
+
   return execute_query($db,$sql,array($history_id["LAST_INSERT_ID()"],$purchase_price,$amount,$item_id));
-  /*set_error(var_dump($history_id));
-  set_error(var_dump($history_id["LAST_INSERT_ID()"]));
-  set_error(var_dump($purchase_price));
-  set_error(var_dump($amount));
-  set_error(var_dump($item_id));
-  return execute_query($db,$sql,array(1,25000,1,1));
-  */
 }
 
 function get_purchase_detail($db,$history_id){
